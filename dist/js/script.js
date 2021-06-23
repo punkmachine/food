fb318f30da22835e5a51153b85d5c2c6e5bd5d07
PunkMashine <76869388+PunkMashine@users.noreply.github.com>
2021-06-23 18:14:22 +0600

    Написал основу табов.
    Скрытие не нужных в данный момент, показ нужного.

--- dist/js/script.js
+++ dist/js/script.js
@@ -94,7 +94,30 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
+ //После загрузки DOM.
 
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach(function (tab) {
+      tab.style.display = 'none';
+    });
+    tabs.forEach(function (tab) {
+      tab.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(number) {
+    tabsContent[number].style.display = 'block';
+    tabs[number].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent(0);
+});
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,26 @@
 'use strict';
 
+//После загрузки DOM.
+document.addEventListener('DOMContentLoaded', () => {
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		  tabsContent = document.querySelectorAll('.tabcontent'),
+		  tabsParent = document.querySelector('.tabheader__items');
+
+	function hideTabContent() {
+		tabsContent.forEach(function(tab) {
+			tab.style.display = 'none';
+		});
+
+		tabs.forEach(function(tab) {
+			tab.classList.remove('tabheader__item_active');
+		});
+	}
+
+	function showTabContent(number) {
+		tabsContent[number].style.display = 'block';
+		tabs[number].classList.add('tabheader__item_active');
+	}
+
+	hideTabContent();
+	showTabContent(0);
+});
