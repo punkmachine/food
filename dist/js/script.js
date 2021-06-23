ad7e470df571f559db2f59d14ef4f9f65daca1e5
PunkMashine <76869388+PunkMashine@users.noreply.github.com>
2021-06-23 18:24:52 +0600

    Добавил переключение табов.

--- dist/js/script.js
+++ dist/js/script.js
@@ -97,9 +97,10 @@
  //После загрузки DOM.
 
 document.addEventListener('DOMContentLoaded', () => {
+  //раздел констант
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
+        tabsParent = document.querySelector('.tabheader__items'); //раздел функций
 
   function hideTabContent() {
     tabsContent.forEach(function (tab) {
@@ -113,7 +114,25 @@ document.addEventListener('DOMContentLoaded', () => {
   function showTabContent(number) {
     tabsContent[number].style.display = 'block';
     tabs[number].classList.add('tabheader__item_active');
-  }
+  } //обработчики событий
+
+
+  tabsParent.addEventListener('click', function (event) {
+    //для того, чтобы часто не писать полностью.
+    const target = event.target; //проверяем клик на нужный элемент
+
+    if (target && target.classList.contains('tabheader__item')) {
+      //для каждого таба, если кликаем, и один из перебираемых элементов
+      //совпадает с тем, на который кликнули, вызываем функцию скрытия 
+      //всех табов и показываем один из них после. 
+      tabs.forEach(function (tab, item) {
+        if (target == tab) {
+          hideTabContent();
+          showTabContent(item);
+        }
+      });
+    }
+  }); //раздел вызова функций
 
   hideTabContent();
   showTabContent(0);
--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@
 
 //После загрузки DOM.
 document.addEventListener('DOMContentLoaded', () => {
+	//раздел констант
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		  tabsContent = document.querySelectorAll('.tabcontent'),
 		  tabsParent = document.querySelector('.tabheader__items');
 
+	//раздел функций
 	function hideTabContent() {
 		tabsContent.forEach(function(tab) {
 			tab.style.display = 'none';
@@ -21,6 +23,26 @@ document.addEventListener('DOMContentLoaded', () => {
 		tabs[number].classList.add('tabheader__item_active');
 	}
 
+	//обработчики событий
+	tabsParent.addEventListener('click', function(event) {
+		//для того, чтобы часто не писать полностью.
+		const target = event.target;
+
+		//проверяем клик на нужный элемент
+		if (target && target.classList.contains('tabheader__item')) {
+			//для каждого таба, если кликаем, и один из перебираемых элементов
+			//совпадает с тем, на который кликнули, вызываем функцию скрытия 
+			//всех табов и показываем один из них после. 
+			tabs.forEach(function(tab, item) {
+				if (target == tab) {
+					hideTabContent();
+					showTabContent(item);
+				}
+			});
+		}
+	});
+
+	//раздел вызова функций
 	hideTabContent();
 	showTabContent(0);
 });
