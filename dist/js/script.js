16d31b8f16ad5cd991ddb936723564fe1c1df8d1
PunkMashine <76869388+PunkMashine@users.noreply.github.com>
2021-06-24 17:16:03 +0600

    Написал таймер на JS.

--- dist/js/script.js
+++ dist/js/script.js
@@ -97,10 +97,11 @@
  //После загрузки DOM.
 
 document.addEventListener('DOMContentLoaded', () => {
-  //раздел констант
+  //! раздел констант
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items'); //раздел функций
+        tabsParent = document.querySelector('.tabheader__items');
+  const deadline = '2021-07-02'; //! раздел функций
 
   function hideTabContent() {
     tabsContent.forEach(function (tab) {
@@ -116,7 +117,59 @@ document.addEventListener('DOMContentLoaded', () => {
     tabsContent[number].classList.add('show', 'fade');
     tabsContent[number].classList.remove('hide');
     tabs[number].classList.add('tabheader__item_active');
-  } //обработчики событий
+  }
+
+  function getTimeRemaining(endtime) {
+    //получение разницы между планируемым временем и текущим
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+          days = Math.floor(t / 86400000),
+          hours = Math.floor(t / 3600000 % 24),
+          minutes = Math.floor(t / 60000 % 60),
+          seconds = Math.floor(t / 1000 % 60); //возвращение значений в виде объекта
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  } //для подставления нуля в таймер, если там стоят не двухзначные дни\часы.
+
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    //получение объектов в которые надо будет запихать разницу
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          //переменная таймера
+    timeInterval = setInterval(updateClock, 1000); //для того, чтобы убрать мигание даты при загрузке страницы.
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime); //занесение необходимых значений в объекты на странице
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds); //остановка таймера.
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  } //! обработчики событий
 
 
   tabsParent.addEventListener('click', function (event) {
@@ -134,10 +187,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
-  }); //раздел вызова функций
+  }); //! раздел вызова функций
 
   hideTabContent();
   showTabContent(0);
+  setClock('.timer', deadline);
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,13 @@
 
 //После загрузки DOM.
 document.addEventListener('DOMContentLoaded', () => {
-	//раздел констант
+	//! раздел констант
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		  tabsContent = document.querySelectorAll('.tabcontent'),
 		  tabsParent = document.querySelector('.tabheader__items');
+	const deadline = '2021-07-02';
 
-	//раздел функций
+	//! раздел функций
 	function hideTabContent() {
 		tabsContent.forEach(function(tab) {
 			tab.classList.add('hide');
@@ -25,7 +26,63 @@ document.addEventListener('DOMContentLoaded', () => {
 		tabs[number].classList.add('tabheader__item_active');
 	}
 
-	//обработчики событий
+	function getTimeRemaining(endtime) {
+		//получение разницы между планируемым временем и текущим
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			  days = Math.floor(t / 86400000),
+			  hours = Math.floor((t / 3600000) % 24),
+			  minutes = Math.floor((t / 60000) % 60),
+			  seconds = Math.floor((t / 1000) % 60);
+		
+		//возвращение значений в виде объекта
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	//для подставления нуля в таймер, если там стоят не двухзначные дни\часы.
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endtime) {
+		//получение объектов в которые надо будет запихать разницу
+		const timer = document.querySelector(selector),
+			  days = timer.querySelector('#days'),
+			  hours = timer.querySelector('#hours'),
+			  minutes = timer.querySelector('#minutes'),
+			  seconds = timer.querySelector('#seconds'),
+			  //переменная таймера
+			  timeInterval = setInterval(updateClock, 1000);
+		
+		//для того, чтобы убрать мигание даты при загрузке страницы.
+		updateClock();
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			//занесение необходимых значений в объекты на странице
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			//остановка таймера.
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	//! обработчики событий
 	tabsParent.addEventListener('click', function(event) {
 		//для того, чтобы часто не писать полностью.
 		const target = event.target;
@@ -44,7 +101,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	//раздел вызова функций
+	//! раздел вызова функций
 	hideTabContent();
 	showTabContent(0);
+	setClock('.timer', deadline);
 });
